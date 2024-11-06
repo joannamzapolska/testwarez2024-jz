f42eac0cdcd6ad4509bb3ed8729b97db3a993239
joannamzapolska <joanna.m.zapolska@gmail.com>
2024-11-06 15:07:02 +0100

    Raporting

--- tests/api.tests.js
+++ tests/api.tests.js
@@ -1,12 +1,15 @@
 import { expect } from "chai";
 import pkg from "pactum";
-import "dotenv/config";
 const { spec } = pkg;
-import {baseURL, userID} from "../helpers/data.js"
+import { baseURL, userID, user, password } from "../helpers/data.js";
+
+let token_response;
+let count;
+let response;
 
 describe("Api tests", () => {
-  it.skip("get request", async () => {
-    const response = await spec().get(`${baseURL}/BookStore/v1/Books`);
+  it.skip("Find a book", async () => {
+    response = await spec().get(`${baseURL}/BookStore/v1/Books`);
     //.inspect();
     expect(response.statusCode).to.eql(200);
 
@@ -22,15 +25,66 @@ describe("Api tests", () => {
     console.log(found);
   });
 
-  it("Authorize a user", async () => {
-    const response = await spec()
+  it("Generate a token", async () => {
+    response = await spec()
       .post(`${baseURL}/Account/v1/GenerateToken`)
       .withBody({
-        userName: "asia123_tw2024",
-        password: process.env.SECRET_PASSWORD,
-      })
-    .inspect()
+        userName: user,
+        password: password,
+      });
     expect(response.statusCode).to.eql(200);
+    expect(response.body.result).to.eql("User authorized successfully.");
+
+    token_response = response.body.token;
   });
 
+  it("post books", async () => {
+    response = await spec()
+      .get(`${baseURL}/Account/v1/User/${userID}/`)
+      .withBearerToken(token_response);
+
+    expect(response.statusCode).to.eql(200);
+    count = response.body.books.length;
+    expect(count).to.eql(1);
+
+    response = await spec()
+      .post(`${baseURL}/BookStore/v1/Books`)
+      .withBearerToken(token_response)
+      .withBody({
+        userId: userID,
+        collectionOfIsbns: [
+          {
+            isbn: "9781449337711",
+          },
+        ],
+      });
+    expect(response.statusCode).to.eql(201);
+
+    response = await spec()
+      .get(`${baseURL}/Account/v1/User/${userID}/`)
+      .withBearerToken(token_response);
+
+    expect(response.statusCode).to.eql(200);
+    count = response.body.books.length;
+    expect(count).to.eql(2);
+  });
+
+  it("delete books", async () => {
+    response = await spec()
+      .delete(`${baseURL}/BookStore/v1/Book`)
+      .withBearerToken(token_response)
+      .withBody({
+        userId: userID,
+        isbn: "9781449337711",
+      });
+    expect(response.statusCode).to.eql(204);
+
+    response = await spec()
+      .get(`${baseURL}/Account/v1/User/${userID}/`)
+      .withBearerToken(token_response);
+
+    expect(response.statusCode).to.eql(200);
+    count = response.body.books.length;
+    expect(count).to.eql(1);
+  });
 });
